f70804ac06093ea78d6e894161141d3940a56592
jmarroquin <mar19845@uvg.edu.gt>
2023-04-20 08:25:46 -0600

    lab c finish

--- main.py
+++ main.py
@@ -1,74 +1,53 @@
-from utils import Utils
+from utils import Utils,Grapher
 import re
 from models import *
 from constants import *
+from NFA import *
+from DFA import *
+from DFA_DIRECT import *
 import uuid
+import sys
 
+# (a|b)*(b|a)*abb
 flag = True
 while flag:
-    print("1. Thompson Algorithm\n2. AFD Subconjuntos\n3. AFD Directo\n4. Exit")
+    
+    print("1. Thompson Algorithm\n2. AFD Subconjuntos\n3. AFD Directo\n4. Yalex\n5. Exit")
     opc=input("Choose an option: ")
     
-    if opc == '4':
-        exit()
-    elif opc == '1':
-        expression,balanced = Utils.get_infix_expression()
-        
-        if balanced is True:
-            expression = Utils.parse_expresion(expression) 
-            expanded_expression = Utils.expand(expression)
-            postfix_expression = Utils.infix_to_postfix(expanded_expression)
-            
-            afn = Utils.thompson_algorithm(postfix_expression)
-            file_name = str(uuid.uuid4().fields[-1])[:5]
-            Utils.write_txt_file(file_name,afn)
-            Utils.graphic(afn,file_name,method='AFN')
-            
-            evaluation_exp = input('Expression to evaluate: ')
-            state = Utils.simulate(evaluation_exp,afn=afn)
-            if state is True:
-                print('The expression: ' + str(evaluation_exp) + ' is valid \n')
-            else:
-                print('The expression: ' + str(evaluation_exp) + ' is not valid \n')
-        else:
-            print('Error in the expression: ' +str(expression) + '\n')
-    elif opc == '2':
-        expression,balanced = Utils.get_infix_expression()
-        if balanced is True:
-            expression = Utils.parse_expresion(expression) 
-            expanded_expression = Utils.expand(expression)
-            postfix_expression = Utils.infix_to_postfix(expanded_expression)
-            afn = Utils.thompson_algorithm(postfix_expression)
-            dfa = Utils.subsets_algorithm(afn)
-            file_name = str(uuid.uuid4().fields[-1])[:5]
-            Utils.write_txt_file(file_name,dfa)
-            Utils.graphic(dfa,file_name,method='AFD')
-            evaluation_exp = input('Expression to evaluate: ')
-            state = Utils.simulate(evaluation_exp,transitions=dfa.transitions,inicial_node=dfa.q0,acceptance_states=dfa.f)
-            if state is True:
-                print('The expression: ' + str(evaluation_exp) + ' is valid \n')
-            else:
-                print('The expression: ' + str(evaluation_exp) + ' is not valid \n')
-        else:
-            print('Error in the expression: ' +str(expression) + '\n')
-            
-    elif opc == '3':
-        expression,balanced = Utils.get_infix_expression()
-        if balanced is True:
-            expression = Utils.parse_expresion(expression) 
-            expanded_expression = Utils.expand(expression)
-            tree = Utils.generate_tree(expanded_expression)
-            dfa = Utils.directo(tree,expanded_expression)
-            file_name = str(uuid.uuid4().fields[-1])[:5]
-            Utils.write_txt_file(file_name,dfa)
-            Utils.graphic(dfa,file_name,method='AFD')
-            evaluation_exp = input('Expression to evaluate: ')
-            state = Utils.simulate(evaluation_exp,transitions=dfa.transitions,inicial_node=dfa.q0,acceptance_states=dfa.f)
-            if state is True:
-                print('The expression: ' + str(evaluation_exp) + ' is valid \n')
-            else:
-                print('The expression: ' + str(evaluation_exp) + ' is not valid \n')
-        else:
-            print('Error in the expression: ' +str(expression) + '\n')
+    if opc == "5":
+        print("By")
+        flag = False
+    elif opc == "1":
+        file_name = Utils.create_filename()
+        postfix_exp = Utils.get_infix_expression()
+        if postfix_exp is not False:
+            nfaBuilt = thompsonBuild(postfix_exp.postfix)
+            nfaBuilt.showNFA(file_name)
+            Grapher.drawNFA(nfaBuilt,file_name)
+            Utils.simulate_exp(nfaBuilt)
+    
+    elif opc == "2":
+        file_name = Utils.create_filename()
+        postfix_exp = Utils.get_infix_expression()
+        if postfix_exp is not False:
+            nfaBuilt = thompsonBuild(postfix_exp.postfix)
+            dfa = DFASubsets(nfaBuilt)
+            newAFD = dfa.buildDFASubsets()
+            newAFD.showSubsetDFA(file_name)
+            Grapher.drawSubsetDFA(newAFD,file_name)
+            Utils.simulate_exp(newAFD)
+    elif opc == "3":
+        file_name = Utils.create_filename()
+        postfix_exp = Utils.get_infix_expression()
+        if postfix_exp is not False:
+            augmented_exp = postfix_exp.augmentRegex()
+            direct = DFAfromTree(augmented_exp)
+            newAFD = direct.buildDFADirect()
+            newAFD.showDFADirect(file_name)
+            Grapher.drawDirectDFA(newAFD,file_name)
+            Utils.simulate_exp(newAFD)
     elif opc !="":
-      print("\nWrong option") 
+      print("\nWrong option")
+    else:
+        break
